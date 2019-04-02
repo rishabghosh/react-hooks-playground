0c3e8cf1b7addfba0bd7aa12522da7f419ad552d
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-04-02 23:02:05 +0530

    Made a simple counter which updates the value when clicked

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const INITIAL_COUNTING_VALUE = 0;
+
+const App = function() {
+  //calling useState with a value will return a array
+  //one element is the current value and other is a function to change the value
+  const [state, setState] = useState(INITIAL_COUNTING_VALUE);
+  return (
+    <main>
+      <div>{state}</div>
+      <button onClick={() => setState(state + 1)}>Increment</button>
+    </main>
+  );
+};
 
 export default App;
