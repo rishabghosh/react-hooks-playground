dbf21b5d84b773dce5ad1b50ca3ea55d32a6ace2
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-04-02 23:07:07 +0530

    made decrement and reset button

--- src/App.js
+++ src/App.js
@@ -7,10 +7,18 @@ const App = function() {
   //calling useState with a value will return a array
   //one element is the current value and other is a function to change the value
   const [state, setState] = useState(INITIAL_COUNTING_VALUE);
+
+  const decrementState = function() {
+    if (state <= 0) return;
+    setState(state - 1);
+  };
+
   return (
     <main>
       <div>{state}</div>
       <button onClick={() => setState(state + 1)}>Increment</button>
+      <button onClick={decrementState}>Decrement</button>
+      <button onClick={()=> setState(INITIAL_COUNTING_VALUE)}>Reset</button>
     </main>
   );
 };
