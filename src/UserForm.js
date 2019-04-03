f36817ee279f893e651adcf74982b6d1cf1fd027
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-04-03 15:49:08 +0530

    Now custom hook useInput is general and is not related to the domain

--- src/UserForm.js
+++ src/UserForm.js
@@ -4,47 +4,42 @@ const EMPTY_STR = "";
 
 /** designing custom hooks */
 const useInput = function() {
-  const [firstName, setFirstName] = useState(EMPTY_STR);
-  const [lastName, setLastName] = useState(EMPTY_STR);
-  const [age, setAge] = useState(EMPTY_STR);
-  const handleFirstNameChange = event => setFirstName(event.target.value);
-  const handleLastNameChange = event => setLastName(event.target.value);
-  const handleAgeChange = event => setAge(event.target.value);
-
-  return {
-    firstName,
-    handleFirstNameChange,
-    lastName,
-    handleLastNameChange,
-    age,
-    handleAgeChange
-  };
+  const [value, setValue] = useState(EMPTY_STR);
+  const onChange = event => setValue(event.target.value);
+  return { value, onChange };
 };
 
 const UserForm = function() {
-  const ui = useInput();
+  /**
+   * destructuring process in the lines below in a es6 feature
+   * where its possible to rename something while destructuring
+   */
+  const { value: firstName, onChange: handleFirstNameChange } = useInput();
+  const { value: lastName, onChange: handleLastNameChange } = useInput();
+  const { value: age, onChange: handleAgeChange } = useInput();
+
   return (
     <form>
       <input
         type="text"
         placeholder="First Name"
-        value={ui.firstName}
-        onChange={ui.handleFirstNameChange}
+        value={firstName}
+        onChange={handleFirstNameChange}
       />
       <input
         type="text"
         placeholder="Last Name"
-        value={ui.lastName}
-        onChange={ui.handleLastNameChange}
+        value={lastName}
+        onChange={handleLastNameChange}
       />
       <input
         type="text"
         placeholder="Age"
-        value={ui.age}
-        onChange={ui.handleAgeChange}
+        value={age}
+        onChange={handleAgeChange}
       />
-      <p>{`Name: ${ui.firstName} ${ui.lastName}`}</p>
-      <p>{`Age: ${ui.age}`}</p>
+      <p>{`Name: ${firstName} ${lastName}`}</p>
+      <p>{`Age: ${age}`}</p>
     </form>
   );
 };
